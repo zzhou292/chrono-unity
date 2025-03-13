831a3566e8f7e1a21e3adb54139951e544084dee
zzhou292@wisc.edu <zzhou292@wisc.edu>
2025-03-13 12:46:39 -0500

    remove plugin

---
+++ Assets/Plugins/chrono_csharp/ChChronoGlobals.cs
@@ -0,0 +1,6 @@
+public static class ChronoGlobals
+{
+    public const string CHRONO_VERSION = "9.0.1";
+    public const string CHRONO_DATA_DIR = "/home/jason/Desktop/chrono-unity/build/data/";
+    public const string CHRONO_VEHICLE_DATA_DIR = "/home/jason/Desktop/chrono-unity/build/data/vehicle/";
+}
